5e0c01db6ad657f3d3b931c4298620e4e0407b81
Χρήστος Σεγκανι <61068732+chrisseg5@users.noreply.github.com>
2023-06-09 18:56:17 +0300

    To Do Api

--- src/main/java/com/example/test2/dto/index/CandidateIndexDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.test2.dto.index;
-
-public class CandidateIndexDto {
-    private Long id;
-    private String name;
-    private  String surName;
-    private String email;
-
-}
--- src/main/java/com/example/test2/service/CandidateService.java
+++
@@ -1,9 +0,0 @@
-package com.example.test2.service;
-
-import com.example.test2.model.Candidate;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface CandidateService {
-    Candidate saveCandidate( Candidate candidate);
-}
